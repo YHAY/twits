0e120292f1ee403415d569d784fc772b06aa2d0f
hi <y83422918@gmail.com>
2021-01-25 23:47:01 +0900

    update join, login, matching ..

--- src/DBManager.py
+++ src/DBManager.py
@@ -1,26 +1,13 @@
-# Module Imports
 import pymysql
 import sys
 import uuid
 
 # import mariadb
 
-# Connect to MariaDB Platform
-try:
-    conn = pymysql.connect(
-        host='localhost',
-        user='admin',
-        password='admin',
-        db='example_db',
-        charset='utf8'
-    )  # 접속정보
 
-except pymysql.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
 
 # Get Cursor
-cur = conn.cursor()
+
 USER = 100
 PROFILE = 95
 GAME = 90
@@ -32,94 +19,172 @@ MATCH = 80
 # pw : 8~16자 영문 대 소문자, 숫자, 특수문자
 # device_num : 20자?
 
-# country : korea(0)
-def create_table(mode):
-    # create table : user, game,
+class DBManager:
+    def __init__(self):
+        # Connect to MariaDB Platform
+        try:
+            self.conn = pymysql.connect(
+                host='localhost',
+                user='admin',
+                password='admin',
+                db='example_db',
+                charset='utf8'
+            )  # 접속정보
 
-    sql = str()
-    if mode == USER:
-        sql = "CREATE TABLE IF NOT EXISTS user (" \
-              " id char(20) UNIQUE, " \
-              " password char(16), " \
-              " name char(20), " \
-              " country int, " \
-              " device char(20) UNIQUE, " \
-              " time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP," \
-              " locked boolean," \
-              " token char(36) UNIQUE" \
-              ")"  # 실행할 sql문 cur.execute(sql) # 커서로 sql문 실행
-    elif mode == PROFILE:
-        sql = "CREATE TABLE IF NOT EXISTS profile (" \
-              " user_token char(36)," \
-              " country int," \
-              " win unsigned int," \
-              " total_count unsigned int," \
-              " photo int," \
-              " time datetime," \
-              "FOREIGN KEY (user_token) REFERENCES user(token)" \
-              ")"  # 실행할 sql문 cur.execute(sql) # 커서로 sql문 실행
-    elif mode == MATCH:
-        sql = "CREATE TABLE IF NOT EXISTS game (" \
-              "id char(4)," \
-              " userName char(10)," \
-              " email char(15)," \
-              " birthYear int" \
-              ")"  # 실행할 sql문 cur.execute(sql) # 커서로 sql문 실행
-    elif mode == GAME:
-        sql = "CREATE TABLE IF NOT EXISTS round (" \
-              "id char(4)," \
-              " userName char(10)," \
-              " email char(15)," \
-              " birthYear int" \
-              ")"  # 실행할 sql문 cur.execute(sql) # 커서로 sql문 실행
-    cur.execute(sql)
-    conn.commit()
+        except pymysql.Error as e:
+            print(f"Error connecting to MariaDB Platform: {e}")
+            sys.exit(1)
 
+        self.cur = self.conn.cursor()
+        # create tables
+        sql = "show tables"
+        self.cur.execute(sql)
+        self.conn.commit()
+        rows = self.cur.fetchall()
+        print("rows :", rows[0])
 
-# insert
-def join(id, pw, name, country, device):
-    try:
-        sql = "INSERT INTO user (id, password, name, country, device, locked, token) values (%s, %s, %s, %s, %s, %s, %s)"
-        val = (id, pw, name, country, device, False, str(uuid.uuid4()))
-        cur.execute(sql, val)
-        rows = cur.fetchall()
-        conn.commit()
-        return True, str()
-    except pymysql.err.IntegrityError as error:
-        # print(error.args[1])
-        return False, error.args[1]
+        new_table_list = [USER, PROFILE, MATCH, GAME]
+        for table in rows[0]:
+            if table == "user":
+                new_table_list.remove(USER)
+            elif table == "profile":
+                new_table_list.remove(PROFILE)
+            elif table == "matches":
+                new_table_list.remove(MATCH)
+            elif table == "game":
+                new_table_list.remove(GAME)
 
+        print("new_table_list : ", new_table_list)
+        for element in new_table_list:
+            print(element)
+            self.create_table(element)
+            # self.create_table(USER)
+            # self.create_table(PROFILE)
+            # self.create_table(MATCH)
+            # self.create_table(GAME)
 
-# select(search)
-def login(id, pw, device):
-    sql = "select token from user where id=%s and password=%s and device=%s"
-    cur.execute(sql, (id, pw, device))
-    # DB결과를 모두 가져올 때 사용
-    rows = cur.fetchall()
-    return rows[0][0]
+    # country : korea(0)
+    def create_table(self, mode):
+        # create table : user, game,
 
-
-def drop_table(mode):
-    try:
-        # drop table
         sql = str()
         if mode == USER:
-            sql = "DROP TABLE user"
+            sql = "CREATE TABLE IF NOT EXISTS user (" \
+                  " id char(20) UNIQUE, " \
+                  " password char(16), " \
+                  " name char(20), " \
+                  " country int, " \
+                  " device char(20) UNIQUE, " \
+                  " time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP," \
+                  " locked boolean," \
+                  " token char(36) UNIQUE" \
+                  ")"  # 실행할 sql문 cur.execute(sql) # 커서로 sql문 실행
         elif mode == PROFILE:
-            sql = "DROP TABLE profile"
-        elif mode == GAME:
-            sql = "DROP TABLE game"
+            sql = "CREATE TABLE IF NOT EXISTS profile (" \
+                  " user_token char(36)," \
+                  " country int," \
+                  " win int unsigned," \
+                  " total_count int unsigned," \
+                  " photo int," \
+                  " time datetime," \
+                  "FOREIGN KEY (user_token) REFERENCES user(token)" \
+                  ")"  # 실행할 sql문 cur.execute(sql) # 커서로 sql문 실행
         elif mode == MATCH:
-            sql = "DROP TABLE match"
-        cur.execute(sql)
-        conn.commit()
-        conn.close()
-    except pymysql.err.OperationalError as error:
-        print(error.args[1])
+            sql = "CREATE TABLE IF NOT EXISTS matches (" \
+                  " user_token char(36)," \
+                  " state int unsigned," \
+                  " matched_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP" \
+                  ")"  # 실행할 sql문 cur.execute(sql) # 커서로 sql문 실행
+        elif mode == GAME:
+            sql = "CREATE TABLE IF NOT EXISTS game (" \
+                  " game_token char(36)," \
+                  " player1 char(36)," \
+                  " player1_choice int," \
+                  " player2 char(36)," \
+                  " player2_choice int," \
+                  " win char(20),"\
+                  " time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP" \
+                  ")"  # 실행할 sql문 cur.execute(sql) # 커서로 sql문 실행
+        print(sql)
+        self.cur.execute(sql)
+        self.conn.commit()
+
+    # insert
+    def join(self, id, pw, name, country, device):
+        try:
+            sql = "INSERT INTO user (id, password, name, country, device, locked, token) values (%s, %s, %s, %s, %s, " \
+                  "%s, %s) "
+            val = (id, pw, name, country, device, False, str(uuid.uuid4()))
+            self.cur.execute(sql, val)
+            rows = self.cur.fetchall()
+            self.conn.commit()
+            return True, str()
+        except pymysql.err.IntegrityError as error:
+            # print(error.args[1])
+            return False, error.args[1]
+
+    # select(search)
+    def login(self, id, pw, device):
+        sql = "select token from user where id=%s and password=%s and device=%s"
+        self.cur.execute(sql, (id, pw, device))
+        # DB결과를 모두 가져올 때 사용
+        rows = self.cur.fetchall()
+        return rows[0][0]
+
+    def del_user(self, user_token):
+        sql = "delete from user where token=%s"
+        val = user_token
+        self.cur.execute(sql, (val))
+        self.conn.commit()
+
+    def drop_table(self, mode):
+        try:
+            # drop table
+            sql = str()
+            if mode == USER:
+                sql = "DROP TABLE IF EXISTS user"
+            elif mode == PROFILE:
+                sql = "DROP TABLE IF EXISTS profile"
+            elif mode == GAME:
+                sql = "DROP TABLE IF EXISTS game"
+            elif mode == MATCH:
+                sql = "DROP TABLE IF EXISTS matches"
+            self.cur.execute(sql)
+            self.conn.commit()
+            # conn.close()
+        except pymysql.err.OperationalError as error:
+            print(error.args[1])
+
+    def get_match(self, user_token):
+        sql = "select user_token from matches where state=0 and user_token!=%s"
+        self.cur.execute(sql, user_token)
+        rows = self.cur.fetchall()
+        self.conn.commit()
+        print("rows:", rows)
+        return rows
+
+    def set_wait(self, user_token):
+        # sql = "update match set "
+        sql = "INSERT INTO matches (user_token, state) values (%s, %s) "
+        val = (user_token, 0)
+        self.cur.execute(sql, val)
+        rows = self.cur.fetchall()
+        self.conn.commit()
+        return rows
+
+    # def update_matching(self, user_token, rival_uuid):
+        #insert game table.
 
 
+# db_manager = DBManager()
+# new_table_list = [PROFILE, USER, MATCH, GAME]
+# for table in new_table_list:
+#     print(table)
+#     db_manager.drop_table(table)
+# conn.close()
 # create_table(USER)
 # join("aaa", "1434", "kkk", 1, "MAC123")
-# drop_table(USER)
-print(login("aaa", "1434", "MAC123"))
+
+# print(db_manager.login("aaa", "1434", "MAC123"))
+# db_manager.get_match("b878c7be-393a-4259-8011-0772154613d9")
 # conn.close()
--- src/api.py
+++ src/api.py
@@ -6,20 +6,32 @@ from DBManager import *
 app = Flask(__name__)
 api = Api(app)
 
+db_manager = DBManager()
+
+
+# 요청을 받으면, json validator먼저 거치도록 만들것.
+
 
 @app.route("/users", methods=["POST"])
 def join():
     user = request.get_json()
     print("user : ", user)
     if "id" and "pw" and "name" and "country" and "device_num" in user:
-        req_result, msg = join(user["id"], user["pw"], user["name"], user["country"], user["device_num"])
+        req_result, msg = db_manager.join(user["id"], user["pw"], user["name"], user["country"], user["device_num"])
         if req_result:
             return {'status': 'success'}
         else:
-            return {'status': 'failed'}
+            return {
+                "error_code": 20003,  # 조금있다가 따로 빼자.
+                "message": "Join-in Failed",
+                "detail": msg
+            }
     else:
-        return {'status': 'failed',
-                'detail': 'not enough request contents'}
+        return {
+            "error_code": 20003,
+            "message": "Join-in Failed",
+            "detail": "Check your request. Not enough requests."
+        }
 
 
 @app.route("/users", methods=["DELETE"])
@@ -28,41 +40,89 @@ def deㅣ_user():
     return {'status': 'success'}
 
 
+# @app.route("/users", methods=["GET"])
+# def user_list():
+#     data = {'status': 'success'}
+#     response = app.response_class(
+#         response=json.dumps(data),
+#         status=200,
+#         mimetype='application/json'
+#     )
+#     return response
+
+
 @app.route("/users", methods=["GET"])
-def user_list():
-    data = {'status': 'success'}
-    response = app.response_class(
-        response=json.dumps(data),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
+def login():
+    user = request.get_json()
+    if not user: # no exist json body.
+        return {
+            "error_code": 20003,
+            "message": "login Failed",
+            "detail": "Check your request. Not enough requests."
+        }
+    if "id" and "pw" and "device_number" in user:
+        token = db_manager.login(user["id"], user["pw"], user["device_number"])
+        resp = {'token': token}
+        response = app.response_class(
+                response=json.dumps(resp),
+                status=200,
+                mimetype='application/json'
+            )
+        return response
+    else: # lack of json body.
+        return {
+            "error_code": 20003,
+            "message": "login Failed",
+            "detail": "Check your request. Not enough requests."
+        }
 
 
 @app.route("/users/logout", methods=["PUT"])
 def logout():
+    # 해당 사용자의 정보를 match테이블에서 지운다.
     return {'status': 'success'}
 
 
-@app.route("/game/matching", methods=["PUT"])
-def matching():
-    return {'status': 'success'}
+@app.route("/game/matching/<user_token>", methods=["PUT"])
+def matching(user_token):
+    # matching table에 올린다. 그리고 match 테이블에서 상대가 있는지 찾아서 resp.
+    # 1. d있다면 상대방의 이름(name), game_token,
+
+    #find rival in match table.
+
+    # wait인 사람이 없으면 대기 테이블로 넘어감.
+    rival_uuid = db_manager.get_match(user_token)
+    # if rival_uuid:
+        # wait인 사람이 있으면 바로 매칭해서 send.
+        # rival_uuid가 1개 일 수도 있고, 아닐 수도 있음.
+
+    # else:
+    #     print(db_manager.set_wait())
+
+    # # insert rival & me
+    # if rival_uuid:
+    #     response = app.response_class(
+    #         response=json.dumps(resp),
+    #         status=200,
+    #         mimetype='application/json'
+    #     )
+    #     return response
 
 
 @app.route("/game/round/<game_token>/choice/<choice_num>", methods=["PUT"])
 def choice(game_token, choice_num):
     return {
         'status': 'success',
-        'round':game_token,
-        'choice_num':choice_num
-        }
+        'round': game_token,
+        'choice_num': choice_num
+    }
 
 
 @app.route("/game/round/<game_token>", methods=["GET"])
 def result(game_token):
     return {
         'status': 'success',
-        'round':game_token
+        'round': game_token
     }
 
 
